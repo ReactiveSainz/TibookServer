b8f092f9f153368867bdd00a06ae3af79b22f4a6
Luis ivan Sainz romero <sainz@SainzAir.local>
2018-09-05 20:18:03 -0500

    added new model resolvers

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,3 +1,11 @@
 import UserResolvers from "./UserResolvers";
+import CreditCardResolvers from "./CreditCardResolvers";
+import PublicationResolvers from "./PublicationResolvers/";
+import TransactionResolvers from "./TransactionResolvers";
 
-export default [UserResolvers];
+export default [
+  UserResolvers,
+  CreditCardResolvers,
+  PublicationResolvers,
+  TransactionResolvers
+];
