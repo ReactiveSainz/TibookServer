85c79a8ed734cfbbac684925a1d020474d2b3db4
Luis ivan Sainz romero <thesainz94@gmail.com>
2018-09-06 18:09:51 -0500

    add autorization on createPublication

--- src/resolvers/PublicationResolvers/Mutations.js
+++ src/resolvers/PublicationResolvers/Mutations.js
@@ -1,19 +1,20 @@
 import { PublicationModel, UserModel } from "../../models";
+import { combineResolvers } from "graphql-resolvers";
+import { isAuthenticated, isAdmin } from "../Authorization";
 
 export default {
-  createPublication: async (
-    parent,
-    { bookISBN, type, price, quantity },
-    { secret, me }
-  ) => {
-    const publication = await new PublicationModel({
-      bookISBN,
-      type,
-      price,
-      quantity,
-      userId: me.id
-    });
-    await publication.save();
-    return publication;
-  }
+  createPublication: combineResolvers(
+    isAuthenticated,
+    async (parent, { bookISBN, type, price, quantity }, { secret, me }) => {
+      const publication = await new PublicationModel({
+        bookISBN,
+        type,
+        price,
+        quantity,
+        userId: me.id
+      });
+      await publication.save();
+      return publication;
+    }
+  )
 };
