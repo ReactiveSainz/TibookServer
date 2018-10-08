4d58904e296f95e28fdec3d6cffcbf7e6ea6cf74
Luis ivan Sainz romero <thesainz94@gmail.com>
2018-10-07 19:26:35 -0500

    retrieve and delete cards, update default card

--- src/schema/CreditCardSchema/CreditCardSchema.js
+++ src/schema/CreditCardSchema/CreditCardSchema.js
@@ -3,9 +3,30 @@ import { gql } from "apollo-server";
 export default gql`
   extend type Mutation {
     createCreditCard(cardToken: String!): CreditCard
+    deleteCreditCard(stripeCardId: ID!): CardDeleted
+    setDefaultCard(stripeCardId: ID!): String
   }
+
+  extend type Query {
+    creditCards: [CreditCardData]
+  }
+
   type CreditCard {
     id: ID!
     stripeCardId: String!
+  }
+
+  type CardDeleted {
+    id: String!
+    deleted: Boolean!
+  }
+
+  type CreditCardData {
+    last4: String!
+    exp_month: Int!
+    exp_year: Int!
+    brand: String!
+    country: String!
+    funding: String!
   }
 `;
