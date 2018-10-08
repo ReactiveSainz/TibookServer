1e089d6639a0f5953e49cfa786b3601015e7e411
Luis ivan Sainz romero <thesainz94@gmail.com>
2018-10-07 19:26:03 -0500

    retrieve cards

--- src/resolvers/CreditCardResolvers/Queries.js
+++ src/resolvers/CreditCardResolvers/Queries.js
@@ -1,1 +1,19 @@
-export default {};
+import { combineResolvers } from "graphql-resolvers";
+import { isAuthenticated, isAdmin } from "../Authorization";
+import { AuthenticationError, ForbiddenError } from "apollo-server";
+import { CreditCardModel } from "../../models";
+import stripePackage from "stripe";
+var stripe = stripePackage("sk_test_ZkfNxv8qYN15b6bWsfqHYPDX");
+
+export default {
+  creditCards: combineResolvers(
+    isAuthenticated,
+    async (parent, { cardToken }, { secret, me }) => {
+      const cards = await stripe.customers.listCards(me.customerId);
+      console.log("cards", cards);
+      if (cards) return cards.data;
+
+      return null;
+    }
+  )
+};
